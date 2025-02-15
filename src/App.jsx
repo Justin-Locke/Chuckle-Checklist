d7abb064b264d687419128f70f886f0a41fabfec
Justin Locke <Justindeanlocke@gmail.com>
2025-02-15 14:08:30 -0600

    Able to create new one liner

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,44 @@
+import { useState } from "react"
+import { useEffect } from "react"
 import "./App.css"
+import { postNewJoke } from "./services/jokeService"
+
 
 export const App = () => {
-  return <div>Hello World!</div>
+
+  const [jokeInput, setJokeInput] = useState()
+  const [savedJoke, setSavedJoke] = useState()
+
+
+  useEffect(() => {
+    if (savedJoke) {
+      postNewJoke(savedJoke)
+    }
+  }, [savedJoke])
+
+  return <>
+    <div className="app-container">
+      <div className="app-heading">
+        <h1 className="app-heading-text">Chuckle Checklist</h1>
+      </div>
+        <h2>Add Joke</h2>
+
+      <div className="joke-add-form">
+        <input
+        className="joke-input"
+        type="text"
+        placeholder="New One Liner"
+        onChange={(event) => {
+          setJokeInput(event.target.value);
+        }}
+      />
+      <button
+        className="joke-input-submit"
+        onClick={() => {
+          setSavedJoke(jokeInput.trimStart())
+        }}>Add</button>
+      </div>
+    
+  </div>
+  </>
 }
---
+++ src/services/jokeService.jsx
@@ -0,0 +1,18 @@
+export const postNewJoke = (newJokeText) => {
+
+    const newJoke = {
+        text: newJokeText,
+        told: false
+    }
+    
+    fetch("http://localhost:8088/jokes", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newJoke)
+    });
+
+    console.log("New Joke Added To Database")
+
+}
